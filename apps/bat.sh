d3c250bfc1a02cb76ba2ccd9c1b7f71876d73465
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-08 23:14:06 +0100

    fix

--- apps/bat.sh
+++ apps/bat.sh
@@ -12,6 +12,10 @@ sudo dpkg -i $temp_dir/bat.deb
 # get config dir from bat --config-dir
 BAT_CONFIG_DIR=$(bat --config-dir)
 curl -L https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme -o $BAT_CONFIG_DIR/themes/
+
+# Build cache so we can get the theme
+bat cache --build
+
 # Clean up
 rm -rf $temp_dir
 
