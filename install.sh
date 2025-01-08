9f8fa53a7c128806ab3b5d894c4e4f169202cfd7
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-08 17:14:21 +0100

    remote ssh for github

--- install.sh
+++ install.sh
@@ -244,6 +244,10 @@ for file in ./apps/*; do
     _task_done
 done
 
+_task "Setting up git remote to ssh one"
+    _cmd "git remote set-url origin git@github.com:helto4real/dev_dotfiles.git"
+_task_done
+
 popd 2>&1 > /dev/null
 
 if ! [[ -f "$IS_FIRST_RUN" ]]; then
