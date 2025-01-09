3070fa035c222a8bbc08b1d1a15f2645a175af40
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-09 23:10:52 +0100

    remove cmp dependency for rust tools

--- config/dotconfig/nvim/lua/helto4real/plugins/dev/rust-tools.lua
+++ config/dotconfig/nvim/lua/helto4real/plugins/dev/rust-tools.lua
@@ -14,9 +14,9 @@ return {
         config = function(_, opts)
             local crates = require('crates')
             crates.setup(opts)
-            require('cmp').setup.buffer({
-                sources = { { name = "crates" } }
-            })
+            -- require('cmp').setup.buffer({
+            --     sources = { { name = "crates" } }
+            -- })
             crates.show()
             require("core.utils").load_mappings("crates")
         end,
