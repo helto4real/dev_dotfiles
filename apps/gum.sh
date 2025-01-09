9994ed96a7aa96ba2b787a96434f34097df13118
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-09 21:13:17 +0100

    Gum

---
+++ apps/gum.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if gum is already installed
+if dpkg -s gum >/dev/null 2>&1; then
+    # gum is already installed
+    return 0
+fi
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install gum
