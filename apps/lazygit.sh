7f868d41895917006689ec14a98374f7b2956d3a
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-09 18:06:04 +0100

    lazygit

---
+++ apps/lazygit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if lazygit is already installed
+if [ -x "$(command -v lazygit)" ]; then
+    # lazygit is already installed
+    return 0
+fi
+
+# Create a temporary directory
+temp_dir=$(mktemp -d)
+
+# Get latest version information
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+
+# Download lazygit to temporary directory
+curl -L lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" -o $temp_dir/lazygit.tar.gz
+# tar xf $temp_dir/lazygit.tar.gz -C $temp_dir/lazygi
+tar -xzf $temp_dir/lazygit.tar.gz -C $temp_dir
+
+# Install lazygit to /usr/local/bin
+sudo install $temp_dir/lazygit -D -t /usr/local/bin/
+
+# Clean up
+rm -rf $temp_dir
