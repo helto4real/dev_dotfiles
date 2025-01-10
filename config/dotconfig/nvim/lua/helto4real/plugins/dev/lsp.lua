0d5eecd12483ada317cb74cb52fa8a9c2264b2a0
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-10 12:13:37 +0100

    changes

--- config/dotconfig/nvim/lua/helto4real/plugins/dev/lsp.lua
+++ config/dotconfig/nvim/lua/helto4real/plugins/dev/lsp.lua
@@ -16,7 +16,14 @@ return {
         { "williamboman/mason-lspconfig.nvim",         event = "VeryLazy" },
         { "WhoIsSethDaniel/mason-tool-installer.nvim", event = "VeryLazy" },
         -- { "jmederosalvarado/roslyn.nvim",              event = "VeryLazy" },
-        { "seblj/roslyn.nvim",                         event = "VeryLazy" },
+        {   
+            "seblj/roslyn.nvim",
+            ft = "cs",
+            opts = {
+                -- your configuration comes here; leave empty for default settings
+            }
+        },
+        -- { "seblj/roslyn.nvim",                         event = "VeryLazy" },
         {
             "olexsmir/gopher.nvim",
             ft = "go",
@@ -190,6 +197,7 @@ return {
                 'gopls',
                 'jsonls',
                 'pyright',
+                -- 'roslyn',
                 -- 'rust_analyzer',
             },
             -- auto-install configured servers (with lspconfig)
@@ -303,30 +311,30 @@ return {
         --     end,
         --     capabilities = capabilities, -- required
         -- })
-        require("roslyn").setup({
-            capabilities = capabilities,
-            config = {
-                -- Here you can pass in any options that that you would like to pass to `vim.lsp.start`
-                -- The only options that I explicitly override are, which means won't have any effect of setting here are:
-                --     - `name`
-                --     - `cmd`
-                --     - `root_dir`
-            },
-            exe = {
-                "dotnet",
-                vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
-            },
-            -- NOTE: Set `filewatching` to false if you experience performance problems.
-            -- Defaults to true, since turning it off is a hack.
-            -- If you notice that the server is _super_ slow, it is probably because of file watching
-            -- I noticed that neovim became super unresponsive on some large codebases, and that was because
-            -- it schedules the file watching on the event loop.
-            -- This issue went away by disabling that capability. However, roslyn will fallback to its own
-            -- file watching, which can make the server super slow to initialize.
-            -- Setting this option to false will indicate to the server that neovim will do the file watching.
-            -- However, in `hacks.lua` I will also just don't start off any watchers, which seems to make the server
-            -- a lot faster to initialize.
-            filewatching = true,
-        })
+        -- require("roslyn").setup({
+        --     capabilities = capabilities,
+        --     config = {
+        --         -- Here you can pass in any options that that you would like to pass to `vim.lsp.start`
+        --         -- The only options that I explicitly override are, which means won't have any effect of setting here are:
+        --         --     - `name`
+        --         --     - `cmd`
+        --         --     - `root_dir`
+        --     },
+        --     exe = {
+        --         "dotnet",
+        --         vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+        --     },
+        --     -- NOTE: Set `filewatching` to false if you experience performance problems.
+        --     -- Defaults to true, since turning it off is a hack.
+        --     -- If you notice that the server is _super_ slow, it is probably because of file watching
+        --     -- I noticed that neovim became super unresponsive on some large codebases, and that was because
+        --     -- it schedules the file watching on the event loop.
+        --     -- This issue went away by disabling that capability. However, roslyn will fallback to its own
+        --     -- file watching, which can make the server super slow to initialize.
+        --     -- Setting this option to false will indicate to the server that neovim will do the file watching.
+        --     -- However, in `hacks.lua` I will also just don't start off any watchers, which seems to make the server
+        --     -- a lot faster to initialize.
+        --     filewatching = true,
+        -- })
     end,
 }
