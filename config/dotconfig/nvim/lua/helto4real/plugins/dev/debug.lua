4e259165ab3e020e2fb9f914db378bfaf8621a80
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-12 10:54:13 +0100

    added f7 for stop debug

--- config/dotconfig/nvim/lua/helto4real/plugins/dev/debug.lua
+++ config/dotconfig/nvim/lua/helto4real/plugins/dev/debug.lua
@@ -105,6 +105,7 @@ return {
             vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
             vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
             vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+            vim.keymap.set('n', '<F7>', dap.terminate, { desc = 'Debug: Terminate' })
             vim.keymap.set('n', '<F9>', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
             vim.keymap.set('n', '<leader>B', function()
                 dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -120,13 +121,13 @@ return {
                 controls = {
                     icons = {
                         pause = '⏸',
-                        play = '▶',
+                        play = '▶️',
                         step_into = '⏎',
                         step_over = '⏭',
                         step_out = '⏮',
                         step_back = 'b',
                         run_last = '▶▶',
-                        terminate = 'T',
+                        terminate = '⏹️',
                         disconnect = 'D',
                     },
                 },
