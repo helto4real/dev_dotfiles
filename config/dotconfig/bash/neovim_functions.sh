dd04310fc888552a187b2c415afd2efd22a7b2ae
Tomas Hellström <tomas.hellstrom@yahoo.se>
2025-01-12 15:12:51 +0100

    Cusomized nssh

--- config/dotconfig/bash/neovim_functions.sh
+++ config/dotconfig/bash/neovim_functions.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-function __nisshfs_usage() {
-    echo -e "${ARROW} ${YELLOW}Usage: nisshfs -s <server> [-d remote_dir]${NC}"
+function __nssh_usage() {
+    echo -e "${ARROW} ${YELLOW}Usage: nssh -s <server> [-d remote_dir]${NC}"
 }
 
-function nisshfs() {
+function nssh() {
     local remote_dir='/'
     local server=''
     local OPTIND=1
     while getopts "hd:s:" opt; do
         case ${opt} in
             h )
-                __nisshfs_usage
+                __nssh_usage
                 return 0
                 ;;
             d )
@@ -22,26 +22,27 @@ function nisshfs() {
                 ;;
             \? )
                 echo -e "${WARNING} ${YELLOW}Invalid option${NC}"
-                __nisshfs_usage
+                __nssh_usage
                 return 1
                 ;;
         esac
     done
     if [ -z $server ]; then
-        __nisshfs_usage
+        __nssh_usage
         return 1
     fi
     if [ ! -d ~/.sshfs ]; then mkdir ~/.sshfs > /dev/null 2>&1; fi
     if [ ! -d ~/.sshfs/$server ]; then mkdir ~/.sshfs/$server > /dev/null 2>&1; fi
-    sshfs -o default_permissions $server:$remote_dir $HOME/.sshfs/$server
+    # sshfs -o default_permissions $server:$remote_dir $HOME/.sshfs/$server
+    sshfs $server:$remote_dir $HOME/.sshfs/$server
     nvim $HOME/.sshfs/$server
     fusermount -zu $HOME/.sshfs/$server
     rm -rf $HOME/.sshfs/$server
 }
 
 
-# give nisshfs ssh tab completion for servers in ~/.ssh/config
-function _nisshfs() {
+# give nssh ssh tab completion for servers in ~/.ssh/config
+function _nssh() {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
@@ -50,4 +51,4 @@ function _nisshfs() {
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     return 0
 }
-complete -F _nisshfs nisshfs
+complete -F _nssh nssh
